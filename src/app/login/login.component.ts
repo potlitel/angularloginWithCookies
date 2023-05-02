c84667eb7a91cf125c4d46a2b7493c496b46fa41
potlitel <potlitel@gmail.com>
2023-05-02 06:49:00 -0400

    The necessary logic is implemented in the login and register components for the correct behavior of the user interface.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor() {}
+
+  login() {
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
