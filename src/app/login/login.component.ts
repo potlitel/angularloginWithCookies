09b81fcff90458582a6d35b517fcf50b879c642a
potlitel <potlitel@gmail.com>
2023-05-09 10:20:18 -0400

    Logic for logging in is incorporated and loading indicator is added to show progress of the call to the endpoint to log in.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { UsersService } from './services/users-service.service';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +10,45 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  //initiate the new Loading variable via BehaviorSubject and set it to "true" from the beginning.
+  public loading$ = new BehaviorSubject<boolean>(false);
 
-  constructor() {}
+  /**
+   * Description
+   * @param {UsersService} publicuserService
+   *  */
+  constructor(public userService: UsersService) {
+    //set the loading$ to true again just before we start the HTTP call
+    this.loading$.next(false);
+  }
 
-  login() {
-    console.log(this.email);
-    console.log(this.password);
+  /**
+   * Description
+   * @returns {any}
+   *  */
+  getLoading(): Observable<boolean> {
+    return this.loading$;
+  }
+
+  /**
+   * Description
+   * @returns {any}
+   *  */
+  login(): void {
+    //set the loading$ to true again just before we start the HTTP call
+    this.loading$.next(true);
+    const user = { email: this.email, password: this.password };
+    this.userService.login(user).subscribe(
+      (data) => {
+        console.log(data);
+      }, //onError method (Blank for now)
+      () => {
+        this.loading$.next(false);
+      },
+      //onComplated method, now that we have the data we can set the loading to false
+      () => {
+        this.loading$.next(false);
+      }
+    );
   }
 }
--- src/app/login/services/users-service.service.ts
+++ src/app/login/services/users-service.service.ts
@@ -1,12 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UsersServiceService {
-  constructor(private http: HttpClient) {}
+export class UsersService {
+  //initiate the new Loading variable via BehaviorSubject and set it to "true" from the beginning.
+  public loading$ = new BehaviorSubject<boolean>(true);
+  
+  constructor(private http: HttpClient) {
+    //set the loading$ to true again just before we start the HTTP call
+    this.loading$.next(true);
+  }
 
   login(user: any): Observable<any> {
     return this.http.post('https://reqres.in/api/login', user);
